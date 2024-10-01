68aa47158a7823bfa97646bb314cbb6a846f9eb8
CollisionDotNet <collisiondotnet@mail.ru>
2024-10-01 18:40:36 +0300

    Для Domain.Entities.CursorInfo добавлен конструктор

--- MouseProcessing.Domain/Entities/CursorInfo.cs
+++ MouseProcessing.Domain/Entities/CursorInfo.cs
@@ -8,5 +8,12 @@ namespace MouseProcessing.Domain.Entities
         public int X { get; }
         public int Y { get; }
         public DateTime Time { get; }
+        public CursorInfo(Guid id,  int x, int y, DateTime time)
+        {
+            Id = id;
+            X = x;
+            Y = y;
+            Time = time;
+        }
     }
 }

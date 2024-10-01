ff742cb654fe8174e1c80d7322a047f25d97b8c8
CollisionDotNet <collisiondotnet@mail.ru>
2024-10-01 17:36:49 +0300

    Добавлен уровень Application
    * Добавлена реализация ICursorInfoService на уровне Application

---
+++ MouseProcessing.Application/Services/CursorInfoService.cs
@@ -0,0 +1,43 @@
+﻿using MouseProcessing.Domain.Abstractions;
+using MouseProcessing.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MouseProcessing.Application.Services
+{
+    public class CursorInfoService : ICursorInfoService
+    {
+        private readonly ICursorInfoRepository _repository;
+        public CursorInfoService(ICursorInfoRepository repository) 
+        { 
+            _repository = repository;
+        }
+        public async Task<Guid> CreateCursorInfoAsync(CursorInfo cursorInfo)
+        {
+            return await _repository.CreateAsync(cursorInfo);
+        }
+
+        public async Task<IEnumerable<Guid>> CreateCursorInfoRangeAsync(IEnumerable<CursorInfo> cursorInfos)
+        {
+            return await _repository.CreateRangeAsync(cursorInfos);
+        }
+
+        public async Task<IEnumerable<CursorInfo>> GetAllCursorInfosAsync()
+        {
+            return await _repository.GetAllAsync();
+        }
+
+        public async Task<CursorInfo?> GetCursorInfoAsync(Guid id)
+        {
+            return await _repository.GetAsync(id);
+        }
+
+        public async Task<IEnumerable<CursorInfo?>> GetCursorInfoRangeAsync(IEnumerable<Guid> ids)
+        {
+            return await _repository.GetRangeAsync(ids);
+        }
+    }
+}

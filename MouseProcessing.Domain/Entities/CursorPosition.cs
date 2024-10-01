cb091d5de974fa5f3b9da9d8ea720522865ff826
CollisionDotNet <collisiondotnet@mail.ru>
2024-10-01 16:44:16 +0300

    Создан проект MouseProcessing.Domain
    * Определена сущность CursorPosition уровня Domain
    * Добавлен файл .gitignore для проектов Visual Studio (https://github.com/github/gitignore/blob/main/VisualStudio.gitignore)

---
+++ MouseProcessing.Domain/Entities/CursorPosition.cs
@@ -0,0 +1,9 @@
+﻿namespace MouseProcessing.Domain.Entities
+{
+    public class CursorPosition
+    {
+        public int X { get; }
+        public int Y { get; }
+        public DateTime Time { get; }
+    }
+}

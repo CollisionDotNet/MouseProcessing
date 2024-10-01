91477701bde244eef67838adfac6813eb9867114
CollisionDotNet <collisiondotnet@mail.ru>
2024-10-01 17:31:34 +0300

    Определены абстракции ICursorInfoRepository и ICursorInfoService

---
+++ MouseProcessing.Domain/Abstractions/ICursorInfoRepository.cs
@@ -0,0 +1,12 @@
+﻿using MouseProcessing.Domain.Entities;
+namespace MouseProcessing.Domain.Abstractions
+{
+    public interface ICursorInfoRepository
+    {
+        public Task<Guid> CreateAsync(CursorInfo cursorInfo);
+        public Task<IEnumerable<Guid>> CreateRangeAsync(IEnumerable<CursorInfo> cursorInfos);
+        public Task<CursorInfo?> GetAsync(Guid id);
+        public Task<IEnumerable<CursorInfo?>> GetRangeAsync(IEnumerable<Guid> ids);
+        public Task<IEnumerable<CursorInfo>> GetAllAsync();
+    }
+}
---
+++ MouseProcessing.Domain/Abstractions/ICursorInfoService.cs
@@ -0,0 +1,18 @@
+﻿using MouseProcessing.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MouseProcessing.Domain.Abstractions
+{
+    public interface ICursorInfoService
+    {
+        public Task<Guid> CreateCursorInfoAsync(CursorInfo cursorInfo);
+        public Task<IEnumerable<Guid>> CreateCursorInfoRangeAsync(IEnumerable<CursorInfo> cursorInfos);
+        public Task<CursorInfo?> GetCursorInfoAsync(Guid id);
+        public Task<IEnumerable<CursorInfo?>> GetCursorInfoRangeAsync(IEnumerable<Guid> ids);
+        public Task<IEnumerable<CursorInfo>> GetAllCursorInfosAsync();
+    }
+}

ee115b3b3bbd8a2fc8305a9c80242e91007bbaec
CollisionDotNet <collisiondotnet@mail.ru>
2024-10-02 16:17:11 +0300

    Метод senddata() в mousetracker.js теперь отслеживает результат операции в виде ответа от сервера

--- MouseProcessing.Client/wwwroot/js/mousetracker.js
+++ MouseProcessing.Client/wwwroot/js/mousetracker.js
@@ -2,17 +2,12 @@
 document.getElementById("trackingbase").addEventListener("mousemove", trackcursor);
 document.getElementById("senddatabtn").addEventListener("click", senddata)
 function trackcursor(event) {
-    console.log(cursorinfos.length);
     cursorinfos.push({ x: event.clientX, y: event.clientY, time: new Date().toJSON() });
 }
 
-async function senddata() {
-    try {
-        console.log(cursorinfos);
-        var response = await axios.post("http://localhost:8080/api/cursorinfos/createrange", cursorinfos);
-        return response.data;
-    } catch (e) {
-        console.error(e);
-    }
+function senddata() {
+    axios.post("http://localhost:8080/api/cursorinfos/createrange", cursorinfos)
+        .then(res => res.status === 200 ? alert("Данные успешно отправлены!") : (alert("Произошла ошибка!"), console.log(res)))
+        .catch(err => (alert("Произошла ошибка!"), console.log(err)));
 }
 
